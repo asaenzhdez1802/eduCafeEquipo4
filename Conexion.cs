b41093c7c2a9b87e0c1d2b202ca3e4cf8038c806
OscarCrs <zcrscortez@gmail.com>
2026-07-16 17:12:51 -0600

    Añade clase Conexion para gestión de base de datos MySQL
    Se incorpora el archivo Conexion.cs con una clase interna que gestiona la conexión a la base de datos MySQL 'edu_cafe', incluyendo manejo de errores con MessageBox. Se actualiza eduCafeEquipo4.csproj para compilar este nuevo archivo.

---
+++ Conexion.cs
@@ -0,0 +1,38 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace eduCafeEquipo4
+{
+    internal class Conexion
+    {
+        //Cadena de conexion a la base de datos con los 5 parametros
+        private readonly string Cadena;
+
+        //constructor
+        public Conexion()
+        {
+            Cadena = "Server=127.0.0.1; Database=edu_cafe; Uid=root; Pwd=; Port=3306;";
+        }
+
+        //metodo para conectar a la base de datos
+        public MySqlConnection GetConexion()
+        {
+            try
+            {
+                MySqlConnection con = new MySqlConnection(Cadena);
+                con.Open();
+                return con;
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show("Error al conectarse:\n" + ex.Message);
+                return null;
+            }
+        }
+    }
+}
