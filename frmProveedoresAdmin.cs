589059b996c8ebd0ad6806c3a311f958da5453d4
OscarCrs <zcrscortez@gmail.com>
2026-07-19 19:21:32 -0600

    Añadir alta de proveedor con validación y duplicados
    Se implementa el evento Click para el botón "Nuevo proveedor", validando campos obligatorios y evitando registros duplicados mediante el método `ExisteProveedor`. Se inserta el proveedor en la base de datos y se actualiza la vista tras el registro exitoso. Se muestran mensajes informativos según el resultado de la operación.

--- frmProveedoresAdmin.cs
+++ frmProveedoresAdmin.cs
@@ -137,5 +137,105 @@ namespace eduCafeEquipo4
                 txtEstado.Text = fila.Cells["estado"].Value.ToString();
             }
         }
+
+        private void btnNuevoProveedor_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtNombreProveedor.Text) |
+                string.IsNullOrWhiteSpace(txtEmpresa.Text) ||
+                string.IsNullOrWhiteSpace(txtCorreo.Text) ||
+                string.IsNullOrWhiteSpace(txtTelefono.Text) ||
+                string.IsNullOrWhiteSpace(txtCalle.Text) ||
+                string.IsNullOrWhiteSpace(txtColonia.Text) ||
+                string.IsNullOrWhiteSpace(txtCiudad.Text) ||
+                string.IsNullOrWhiteSpace(txtCodigoPostal.Text) ||
+                string.IsNullOrWhiteSpace(txtEstado.Text))
+            {
+                MessageBox.Show("Por favor, completa todos los campos.", "Campos vacíos", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (ExisteProveedor(txtNombreProveedor.Text.Trim(), txtCorreo.Text.Trim(), txtTelefono.Text.Trim()))
+            {
+                MessageBox.Show("El proveedor ya existe en la base de datos.", "Proveedor existente", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+
+            Conexion conClase = new Conexion();
+            MySqlConnection dbConn = conClase.GetConexion();
+
+            //Si falla la conexion
+            if (dbConn == null) return;
+
+            try
+            {
+                string sql = "INSERT INTO proveedor (nombre_proveedor, empresa, correo, telefono, calle, colonia, ciudad, codigo_postal, estado) VALUES (@nombre_proveedor, @empresa, @correo, @telefono, @calle, @colonia, @ciudad, @codigo_postal, @estado)";
+                MySqlCommand cmd = new MySqlCommand(sql, dbConn);
+
+                cmd.Parameters.AddWithValue("@nombre_proveedor", txtNombreProveedor.Text.Trim());
+                cmd.Parameters.AddWithValue("@empresa", txtEmpresa.Text.Trim());
+                cmd.Parameters.AddWithValue("@correo", txtCorreo.Text.Trim());
+                cmd.Parameters.AddWithValue("@telefono", txtTelefono.Text.Trim());
+                cmd.Parameters.AddWithValue("@calle", txtCalle.Text.Trim());
+                cmd.Parameters.AddWithValue("@colonia", txtColonia.Text.Trim());
+                cmd.Parameters.AddWithValue("@ciudad", txtCiudad.Text.Trim());
+                cmd.Parameters.AddWithValue("@codigo_postal", txtCodigoPostal.Text.Trim());
+                cmd.Parameters.AddWithValue("@estado", txtEstado.Text);
+                int filas = cmd.ExecuteNonQuery();
+                dbConn.Close();
+
+                if (filas > 0)
+                {
+                    MessageBox.Show("Proovedor registrado con éxito.", "Guardar", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                    CargarProveedores();
+
+                    txtNombreProveedor.Clear();
+                    txtEmpresa.Clear();
+                    txtCorreo.Clear();
+                    txtTelefono.Clear();
+                    txtCalle.Clear();
+                    txtColonia.Clear();
+                    txtCiudad.Clear();
+                    txtCodigoPostal.Clear();
+                    txtEstado.SelectedIndex = -1;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al registrar el Proveedor: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                if (dbConn.State == System.Data.ConnectionState.Open)
+                {
+                    dbConn.Close();
+                }
+            }
+        }
+
+        private bool ExisteProveedor(string nombre, string correo, string telefono)
+        {
+            Conexion conClase = new Conexion();
+            MySqlConnection dbConn = conClase.GetConexion();
+
+            if (dbConn == null) return false;
+
+            try
+            {
+                string sql = "SELECT COUNT(*) FROM proveedor WHERE nombre_proveedor = @nombre OR coreeo = @correo OR telefono =@telefono";
+                MySqlCommand cmd = new MySqlCommand(sql, dbConn);
+                cmd.Parameters.AddWithValue("@nombre", nombre);
+                cmd.Parameters.AddWithValue("@correo", correo);
+                cmd.Parameters.AddWithValue("@telefono", telefono);
+
+                int cantidad = Convert.ToInt32(cmd.ExecuteScalar());
+                dbConn.Close();
+
+                return cantidad > 0;
+            }
+            catch
+            {
+                if (dbConn.State == System.Data.ConnectionState.Open) dbConn.Close();
+                return false;
+            }
+        }
     }
 }
